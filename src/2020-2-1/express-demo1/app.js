99164d39ecdd287c1d7395e1f110e95efb642600
Ivens_Zhang <49118158+Ivens-Zhang@users.noreply.github.com>
2020-02-02 15:31:58 +0800

    express art-template init

--- src/2020-2-1/express-demo1/app.js
+++ src/2020-2-1/express-demo1/app.js
@@ -4,9 +4,36 @@ var app = express()
 
 app.use('/public/', express.static('./public'))
 
-app.get('/', function () {  
-  app.send('Hello Node')
+// view engine setup
+app.engine('html', require('express-art-template'));
+
+// app.set('view', {
+//     debug: process.env.NODE_ENV !== 'production'
+// });
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'art');
+
+// // routes
+// app.get('/', function (req, res) {
+//     res.render('index.art', {
+//         user: {
+//             name: 'aui',
+//             tags: ['art', 'template', 'nodejs']
+//         }
+//     });
+// });
+
+app.get('/', function (req, res) {  
+  // 如果不对配置进行修改, 则默认从 views 文件夹中寻找文件
+  res.render('index.html')
 })
+
+app.get('/404', function (req, res) { 
+  console.log(req.query); 
+  var userInfo = req.query
+  res.render('404.html')
+})
+
 
 app.listen('3000', function () {  
   console.log('正在侦听 3000 端口');
