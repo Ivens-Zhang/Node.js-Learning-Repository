0098315b012979aabc1cd5c3ffb6d044a39b88cc
Ivens_Zhang <49118158+Ivens-Zhang@users.noreply.github.com>
2020-02-04 14:31:12 +0800

    学生系统 crud 简单 demo 完成

--- src/2020-2-2/crud-express/router.js
+++ src/2020-2-2/crud-express/router.js
@@ -4,6 +4,9 @@ var Student = require('./student')
 
 var router = express.Router()
 
+/**
+ * 学生后台首页
+ */
 router.get('/students', function (req, res) {  
   Student.find(function (err, students) {  
     if (err) {
@@ -14,24 +17,18 @@ router.get('/students', function (req, res) {
       students
     })
   })
-  
-  // fs.readFile('./db.json', 'utf8', function (err, data) {  
-  //   if (err) {
-  //     return console.log('数据库读取失败');
-  //   }
-  //   // console.log(data);
-  //   var students = JSON.parse(data).students
-  //   res.render('index.html', {
-  //     fruits: ['苹果', '香蕉', '橘子', '菠萝'],
-  //     students
-  //   })
-  // })
 })
 
-
+/**
+ * 进入新增学生页面
+ */
 router.get('/students/new', function (req, res) {  
   res.render('new.html')
 })
+
+/**
+ *  新增完毕点击确认发送数据
+ */
 router.post('/students/new', function (req, res) {  
   console.log( req.body );
   var student = req.body
@@ -42,14 +39,44 @@ router.post('/students/new', function (req, res) {
   }) 
   res.redirect('/students')
 })
+
+/**
+ *  进入修改学生信息页面
+ */
 router.get('/students/edit', function (req, res) {  
-
+  Student.findById(Number(req.query.id), function (err, student) {  
+    if (err) {
+      console.log(err);
+    }
+    res.render('edit.html', {
+      student
+    })
+  })
 })
-router.post('/students/edit', function (req, res) {  
 
+/**
+ *  修改完成确认发送信息
+ */
+router.post('/students/edit', function (req, res) { 
+  Student.edit(req.body, function (err) {  
+    if (err) {
+      return console.log("2222222222222");
+    }
+  })
+  res.redirect('/students')
 })
+
+/**
+ *  删除学生
+ */
 router.get('/students/delete', function (req, res) {  
-
+  console.log(req.query.id);
+  Student.remove(req.query.id, function (err) {  
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/students')
+  })
 })
 
 module.exports = router
--- src/2020-2-2/crud-express/student.js
+++ src/2020-2-2/crud-express/student.js
@@ -41,3 +41,82 @@ exports.save = function (student, callback) {
     })
   })
 }
+
+/**
+ * 确定修改学生信息
+ */
+exports.edit = function (student, callback) {  
+  fs.readFile(dbPath, 'utf8', function (err, data) {  
+    if (err) {
+      callback(err)
+    }
+    var students = JSON.parse(data).students
+
+    student.id = parseInt(student.id)
+    student.age = parseInt(student.age)
+    student.gender = parseInt(student.gender)
+    
+    var stu = students.find(function (item) {  
+      return item.id === student.id
+    })
+    
+    for (var key in student) {
+      stu[key] = student[key]
+    }
+
+    // 把对象数据转换为字符串
+    var fileData = JSON.stringify({students})
+    
+    // 将字符串写入 db.json
+    fs.writeFile(dbPath, fileData, function (err) {  
+      if (err) {
+        return console.log('数据库写入失败');
+      }
+    })
+  })
+}
+
+/**
+ *  按照 id 获取特定学生信息
+ */
+exports.findById = function (id, callback) { 
+  fs.readFile(dbPath, 'utf8', function (err, data) {  
+    if (err) {
+      return console.log(err);
+    }
+    var students = JSON.parse(data).students
+    var student = students.find(function (item) {
+      console.log(id + '!!!')
+      return item.id === id
+    })
+    callback(null, student)
+  })
+}
+
+/**
+ *  根据 id 删除学生信息
+ */
+exports.remove = function (id, callback) {  
+  fs.readFile(dbPath, 'utf8', function (err, data) {  
+    if (err) {
+      callback(err)
+    }
+    var students = JSON.parse(data).students
+    var studentIndex = students.findIndex(function (item) {  
+      return item.id === parseInt(id)
+    })
+    students.splice(studentIndex, 1)
+    
+    // 把对象数据转换为字符串
+    var fileData = JSON.stringify({students})
+    
+    // 将字符串写入 db.json
+    fs.writeFile(dbPath, fileData, function (err) {  
+      if (err) {
+        return console.log('数据库写入失败');
+      }
+      callback(null)
+    })
+    
+  })
+}
