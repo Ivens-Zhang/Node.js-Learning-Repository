aaa9c876d91948982b53b2d07486414ff93dd6e2
Ivens_Zhang <49118158+Ivens-Zhang@users.noreply.github.com>
2020-02-04 17:49:36 +0800

    mongo-demo1 init

---
+++ src/2020-2-4/mongoose-demo1/demo1.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+// mongoose.Promise = global.Promise;
+
+// var Cat = mongoose.model('Cat', { name: String });
+
+// var kitty = new Cat({ name: 'Zildjian' });
+// kitty.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('meow');
+//   }
+// });
